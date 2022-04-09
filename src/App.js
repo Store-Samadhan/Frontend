7ab12b631882072794b2294b8f932704217d09b8
Raj <varasaniraj5@gmail.com>
2022-04-10 00:32:52 +0530

    Set up react filepond

--- src/App.js
+++ src/App.js
@@ -2,11 +2,18 @@ import React from "react";
 import { useSelector } from "react-redux";
 import { Routes, Route } from "react-router-dom";
 import { ToastContainer } from "react-toastify";
+import { registerPlugin } from "react-filepond";
 
 import styles from "./App.module.css";
 import "react-toastify/dist/ReactToastify.css";
+import "filepond/dist/filepond.min.css";
+
+import FilePondPluginImageExifOrientation from "filepond-plugin-image-exif-orientation";
+import FilePondPluginImagePreview from "filepond-plugin-image-preview";
+import "filepond-plugin-image-preview/dist/filepond-plugin-image-preview.css";
 
 import LandingPage from "./Containers/LandingPage";
+registerPlugin(FilePondPluginImageExifOrientation, FilePondPluginImagePreview);
 
 const App = () => {
   const userData = useSelector((state) => state.userReducer.userData);
