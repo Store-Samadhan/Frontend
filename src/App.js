2c7259d9d9f17208fde1d144460730d0dee37098
Raj <varasaniraj5@gmail.com>
2022-04-10 00:23:30 +0530

    Setup react toastify
    Co-authored-by: Ujjawal Gupta <UjjawalGupta30@users.noreply.github.com>

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
 import React from "react";
 import { useSelector } from "react-redux";
 import { Routes, Route } from "react-router-dom";
+import { ToastContainer } from "react-toastify";
 
 import styles from "./App.module.css";
+import "react-toastify/dist/ReactToastify.css";
 
 import LandingPage from "./Containers/LandingPage";
 
@@ -11,6 +13,7 @@ const App = () => {
 
   return (
     <>
+      <ToastContainer bodyClassName={styles.ToastBody} />
       {userData != null && (
         <Routes>
           {["/", "login", "signup"].map((path, index) => (
