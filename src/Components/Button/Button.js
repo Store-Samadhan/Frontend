227e2889e809172c4d0d9259d3b896dc09ae551c
Akash Gupta <akash.gupta.anup@gmail.com>
2022-04-09 20:51:37 +0530

    Customized Button Component
    Co-authored-by: Ujjawal Gupta <UjjawalGupta30@users.noreply.github.com>

--- src/Components/Button/Button.js
+++ src/Components/Button/Button.js
@@ -2,12 +2,47 @@ import React from "react";
 
 import styles from "./Button.module.css";
 
-const Button = ({ title, onClick }) => {
+function Button({
+  name,
+  onClick,
+  primaryColor,
+  inverted,
+  wrapperClass,
+  id,
+  withIcon,
+  IconComp,
+  iconClass,
+  hoverBgColor,
+  hoverColor,
+  reverseFlex,
+  empty,
+  fullWidth,
+}) {
   return (
-    <button onClick={onClick} className={styles.button}>
-      {title}
+    <button
+      id={id}
+      className={
+        styles.Button +
+        " " +
+        (inverted || empty ? styles.Inverted : "") +
+        " " +
+        (empty ? styles.Empty : "") +
+        " " +
+        wrapperClass
+      }
+      style={{
+        "--main-color": primaryColor,
+        "--main-hover-bg-color": hoverBgColor ? hoverBgColor : "none",
+        "--main-hover-color": hoverColor ? hoverColor : primaryColor,
+        flexDirection: reverseFlex ? "row-reverse" : "row",
+        width: fullWidth ? "100%" : "fit-content",
+      }}
+      onClick={onClick}
+    >
+      {withIcon && <IconComp className={styles.Icon + " " + iconClass} />}{" "}
+      {name}
     </button>
   );
-};
+}
 
 export default Button;
