de8eb207bb9b507f75a81c4ff1d2271923e07a49
Akash Gupta <akash.gupta.anup@gmail.com>
2022-04-09 20:57:17 +0530

    Added Material UI setup Added global css variables Added Landing Page Route
    Co-authored-by: Ujjawal Gupta <UjjawalGupta30@users.noreply.github.com>

--- src/App.js
+++ src/App.js
@@ -1,19 +1,25 @@
 import React from "react";
+import { useSelector } from "react-redux";
+import { Routes, Route } from "react-router-dom";
 
-import { Switch, Route } from "react-router-dom";
+import styles from "./App.module.css";
 
-import Home from "./Containers/Home";
+import LandingPage from "./Containers/LandingPage";
 
 const App = () => {
-  
-    return (
-      <Switch>
-        <Route exact path="/">
-          <Home />
-        </Route>
-      </Switch>
-    )
-  
+  const userData = useSelector((state) => state.userReducer.userData);
+
+  return (
+    <>
+      {userData != null && (
+        <Routes>
+          {["/", "login", "signup"].map((path, index) => (
+            <Route exact key={index} path={path} element={<LandingPage />} />
+          ))}
+        </Routes>
+      )}
+    </>
+  );
 };
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -1,38 +1,36 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
+import React from "react";
+import ReactDOM from "react-dom";
+import { ThemeProvider, createTheme } from "@mui/material/styles";
 
 import { BrowserRouter as Router } from "react-router-dom";
 
-
 import { Provider } from "react-redux";
 
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
-
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
 
 import { ConfigureStore } from "./Redux/ConfigureStore";
 
 const store = ConfigureStore();
 
+const globalMUITheme = createTheme({
+  typography: {
+    fontFamily: ["Poppins", "Roboto", "sans-serif"].join(","),
+  },
+});
 
 ReactDOM.render(
   <React.StrictMode>
-    
+    <ThemeProvider theme={globalMUITheme}>
       <Provider store={store}>
-    
-      
-      <Router>
-      
-        <App />
-      
-      </Router>
-      
-    
+        <Router>
+          <App />
+        </Router>
       </Provider>
-    
+    </ThemeProvider>
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
 
 // If you want to start measuring performance in your app, pass a function
