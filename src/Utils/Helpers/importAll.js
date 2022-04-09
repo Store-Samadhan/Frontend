89673140b7ba7238021c8bcf4ab5892d984a1949
Raj <varasaniraj5@gmail.com>
2022-04-09 23:54:07 +0530

    Created import all helper function
    Co-authored-by: Ujjawal Gupta <UjjawalGupta30@users.noreply.github.com>

---
+++ src/Utils/Helpers/importAll.js
@@ -0,0 +1,9 @@
+function importAll(r) {
+  let images = {};
+  r.keys().map((item, index) => {
+    images[item.replace("./", "")] = r(item);
+  });
+  return images;
+}
+
+export default importAll;
