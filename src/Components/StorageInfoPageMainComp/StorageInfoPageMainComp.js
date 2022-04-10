5daca00da718a7320e168651feaafb5fa49adf6a
Akash Gupta <akash.gupta.anup@gmail.com>
2022-04-10 13:46:39 +0530

    Added StorageInfoPageMainsection
    Co-authored-by: Ujjawal Gupta <UjjawalGupta30@users.noreply.github.com>

---
+++ src/Components/StorageInfoPageMainComp/StorageInfoPageMainComp.js
@@ -0,0 +1,33 @@
+import React, { useState } from "react";
+
+import styles from "./StorageInfoPageMainComp.module.css";
+
+import StorageInfoImageSec from "./StorageInfoImageSec";
+import StorageInfoInfoSec from "./StorageInfoInfoSec";
+import StorageInfoReviewSec from "./StorageInfoReviewSec";
+
+function StorageInfoPageMainComp({ storageDetails, refreshDataFun }) {
+  return (
+    <div className={styles.Wrapper}>
+      <div className={styles.UpperSection}>
+        <div className={styles.LeftSecWrapper}>
+          <StorageInfoImageSec
+            images={storageDetails.images}
+            googlEmbedURL={storageDetails.mapsEmbedURL}
+          />
+        </div>
+        <div className={styles.RightSecWrapper}>
+          <StorageInfoInfoSec storageDetails={storageDetails} />
+        </div>
+      </div>
+      <div className={styles.LowerSection}>
+        <StorageInfoReviewSec
+          storageDetails={storageDetails}
+          refreshDataFun={refreshDataFun}
+        />
+      </div>
+    </div>
+  );
+}
+
+export default StorageInfoPageMainComp;
