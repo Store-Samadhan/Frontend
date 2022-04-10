ca44f4b71be2c84ba390ec05fdb4f0db527253f2
Akash Gupta <akash.gupta.anup@gmail.com>
2022-04-10 17:13:23 +0530

    Added constants
    Co-authored-by: Ujjawal Gupta <UjjawalGupta30@users.noreply.github.com>

--- src/Utils/Constants/APIConstants.js
+++ src/Utils/Constants/APIConstants.js
@@ -1,7 +1,8 @@
-export const BASE_URL =
-  "http://localhost:8000/api/v1";
+export const BASE_URL = "http://localhost:8000/api/v1";
 export const AUTH_URL = `${BASE_URL}/auth`;
+export const STORAGE_URL = `${BASE_URL}/storage`;
 
 export const SIGNUP_USER_URL = `${AUTH_URL}/signupuser`;
 export const SIGNUP_STORAGE_URL = `${AUTH_URL}/signupStorage`;
 export const GET_USER_URL = `${AUTH_URL}/getUserDetails`;
+export const GET_FILTERED_STORAGE_URL = `${STORAGE_URL}/getFilteredStorage`;
