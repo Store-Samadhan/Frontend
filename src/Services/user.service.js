87f5be6dde84611b9d80baa7114c4cb3e806fdf5
Raj <varasaniraj5@gmail.com>
2022-04-10 20:06:42 +0530

    Created service for user data updation
    Co-authored-by: Ujjawal Gupta <UjjawalGupta30@users.noreply.github.com>

--- src/Services/user.service.js
+++ src/Services/user.service.js
@@ -1,6 +1,10 @@
 import axios from "axios";
 
-import { GET_USER_URL, ADD_REVIEW_URL } from "../Utils/Constants/APIConstants";
+import {
+  GET_USER_URL,
+  ADD_REVIEW_URL,
+  UPDATE_USER_DATA_URL,
+} from "../Utils/Constants/APIConstants";
 
 export const getUser = async (accessToken) => {
   try {
@@ -20,6 +24,23 @@ export const addReview = async (review, accessToken) => {
     const { data } = await axios.post(
       `${ADD_REVIEW_URL}`,
       { ...review },
+      {
+        headers: {
+          Authorization: `Bearer ${accessToken}`,
+        },
+      }
+    );
+    return data;
+  } catch (err) {
+    console.log(err);
+  }
+};
+
+export const updateUser = async (userData, accessToken) => {
+  try {
+    const { data } = await axios.put(
+      `${UPDATE_USER_DATA_URL}`,
+      { userData },
       {
         headers: {
           Authorization: `Bearer ${accessToken}`,
