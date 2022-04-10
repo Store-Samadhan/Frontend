6dd0acdbf255af68fdd61d4c5d7d41f4fe8ea827
Raj <varasaniraj5@gmail.com>
2022-04-10 15:48:26 +0530

    Integrated sign up
    Co-authored-by: Ujjawal Gupta <UjjawalGupta30@users.noreply.github.com>

--- src/Utils/Constants/APIConstants.js
+++ src/Utils/Constants/APIConstants.js
@@ -1,1 +1,7 @@
-export const BASE_URL = "https://store-samadhan-backend.centralindia.cloudapp.azure.com/api/v1";
+export const BASE_URL =
+  "http://localhost:8000/api/v1";
+export const AUTH_URL = `${BASE_URL}/auth`;
+
+export const SIGNUP_USER_URL = `${AUTH_URL}/signupuser`;
+export const SIGNUP_STORAGE_URL = `${AUTH_URL}/signupStorage`;
+export const GET_USER_URL = `${AUTH_URL}/getUserDetails`;
